ab980cba38eb4e58b67a8dbdf2ded573b0e7de10
grzesieklog <gk180984@interia.pl>
2022-08-23 08:44:00 +0200

    Clean code, add max -g val

--- ifrtstat.c
+++ ifrtstat.c
@@ -1,6 +1,4 @@
-// gcc ifrtstat.c -o ifrtstat -lgmp
 #include <stdio.h>
-//#include <stdint.h>
 #include <inttypes.h>
 #include <string.h>
 #include <stdlib.h>
@@ -19,21 +17,14 @@
 #include <netlink/route/link.h>
 
 #define PROG_NAME "ifrtstat"
-#define IF_LEN 16
-/*#define IF_DIR "/sys/class/net/"
-#define IF_PATH_R "/statistics/rx_bytes"
-#define IF_PATH_T "/statistics/tx_bytes"*/
+#define MAX_IF_LEN 16
+#define MAX_GOPT_LEN 20
 #define DAY_IN_SEC 60*60*24
-
+#define UINTMAX_MAX_AS_STR "18446744073709551615"
 
 time_t now;
 char date_t[32];
-//int filedr=0;
-//int filedt=0;
 char *interface;
-/*char testpath[64];
-char rxpath[64];
-char txpath[64];*/
 
 struct rtnl_link *nlink;
 struct nl_sock *nlsocket;
@@ -48,12 +39,12 @@ mpz_t r,t,sr,st;
 mpz_t _r,_t,_sr,_st;
 mpz_t rem_r,rem_t,rem_sr,rem_st;
 
-unsigned char f_int=0,
-              f_max=0,
-              f_date=0,
-              f_timer=0,
-              f_greater=0,
-              f_help=0;
+uint8_t f_int=0,
+        f_max=0,
+        f_date=0,
+        f_timer=0,
+        f_greater=0,
+        f_help=0;
 
 void sig_stop(int sig);
 void get_time();
@@ -68,8 +59,8 @@ int main(int argc, char* argv[]) {
   const char options[] = "hmdtg:";
   char *greater;
   struct stat fstat;
-  unsigned char flag_sum=0;
-  unsigned char int_sum=0;
+  uint8_t flag_sum=0;
+  uint8_t int_sum=0;
   // get options
   while ((opt=getopt(argc,argv,options)) != -1) {
     switch (opt) {
@@ -110,6 +101,10 @@ int main(int argc, char* argv[]) {
           printf("Option -g is duplicated.\n");
           exit(7);
         }
+        if (strlen(optarg)>MAX_GOPT_LEN){
+          printf("Value of -g option is too long.\n");
+          exit(7);
+        }
         // TODO clear '=' from val
         greater = optarg;
         f_greater=1;
@@ -134,42 +129,16 @@ int main(int argc, char* argv[]) {
     if (f_greater)
       if (strcmp(argv[n],greater)==0)
         continue;
-    // TODO len of -g val, now is 999..PB
-    if (strlen(argv[n])>IF_LEN){
+    if (strlen(argv[n])>MAX_IF_LEN){
       printf("Too long arg: %s\n",argv[n]);
       exit(10);
     }
     if (argv[n][0]=='-') continue;
-    /*
-    memset(testpath,0,sizeof(testpath));
-    strcat(testpath,IF_DIR);
-    strcat(testpath,argv[n]);
-    */
-    //if (stat(testpath,&fstat)==0){ // if exist?
     if (rtnl_link_get_kernel(nlsocket,0,argv[n],&nlink)==0){ // if exist?
       rtnl_link_put(nlink);
       interface = argv[n];
       f_int=1;
       int_sum++;
-      /*
-      if (S_ISDIR(fstat.st_mode)) {
-        interface = argv[n];
-        memset(rxpath,0,sizeof(rxpath));
-        memset(txpath,0,sizeof(txpath));
-        strcat(rxpath,IF_DIR);
-        strcat(txpath,IF_DIR);
-        strcat(rxpath,interface);
-        strcat(txpath,interface);
-        strcat(rxpath,IF_PATH_R);
-        strcat(txpath,IF_PATH_T);
-        if (access(rxpath,R_OK)==0 && access(txpath,R_OK)==0){
-          f_int=1;
-          int_sum++;
-        } else {
-          printf("Cannot read:\n%s\n%s\n",rxpath,txpath);
-          exit(11);
-        }
-      }*/
     }
     else{
       printf("unknown arg: %s\n",argv[n]);
@@ -208,14 +177,7 @@ int main(int argc, char* argv[]) {
   signal(SIGKILL, sig_stop);
   signal(SIGQUIT, sig_stop);
   signal(SIGTERM, sig_stop);
-  /*signal(SIGTSTP, sig_stop);
-  filedr = open(rxpath, O_RDONLY );
-  filedt = open(txpath, O_RDONLY );
-
-  if( !filedr || !filedt ) {
-    printf("Open file error\n");
-    exit(13);
-  }*/
+  
   alloc_num();
   mpz_set_str(kB,"1000",10);
   mpz_set_str(MB,"1000000",10);
@@ -241,12 +203,11 @@ int main(int argc, char* argv[]) {
   if (f_greater){
     mpz_set_str(min_Bps,greater,10);
   }
-  char rxbuf[128];
-  char txbuf[128];
-  //int nbytesr=0, nbytest=0;*/
-  unsigned long int ui_hours=0,ui_minutes=0,ui_sec=0;
-  char newrxmax=0,newtxmax=0;
-  char bigr=0,bigt=0,bigsr=0,bigst=0;
+  char rxbuf[strlen(UINTMAX_MAX_AS_STR)+2];
+  char txbuf[strlen(UINTMAX_MAX_AS_STR)+2];
+  uint32_t ui_hours=0,ui_minutes=0,ui_sec=0;
+  uint8_t newrxmax=0,newtxmax=0;
+  uint8_t bigr=0,bigt=0,bigsr=0,bigst=0;
   char rj[6],tj[6],srj[4],stj[4];
   char printrt;
   if (!f_date){
@@ -255,11 +216,6 @@ int main(int argc, char* argv[]) {
   }
   while(1) {
     printrt=1;
-    /*memset(rxbuf,0,sizeof(rxbuf));
-    memset(txbuf,0,sizeof(txbuf));
-    nbytesr = read(filedr, rxbuf, 128);
-    nbytest = read(filedt, txbuf, 128);*/
-    //if(nbytesr > 0 && nbytest > 0) {
     if(rtnl_link_get_kernel(nlsocket,0,interface,&nlink)==0){
       rx_bytes=rtnl_link_get_stat(nlink,RTNL_LINK_RX_BYTES);
       tx_bytes=rtnl_link_get_stat(nlink,RTNL_LINK_TX_BYTES);
@@ -301,56 +257,57 @@ int main(int argc, char* argv[]) {
       if (f_greater){
         if (mpz_cmp(r,min_Bps)<0 && mpz_cmp(t,min_Bps)<0) printrt=0;
       }
-      // TODO don't calculate if not printrt
       // cal units
-      if (mpz_cmp(sr,PBborder)>0)
-        { mpz_fdiv_qr(sr,rem_sr,sr,PB); mpz_fdiv_q(rem_sr,rem_sr,TB); strcpy(srj,"PB"); bigsr=1;}
-      else if (mpz_cmp(sr,TBborder)>0)
-        { mpz_fdiv_qr(sr,rem_sr,sr,TB); mpz_fdiv_q(rem_sr,rem_sr,GB); strcpy(srj,"TB"); bigsr=1;}
-      else if (mpz_cmp(sr,GBborder)>0)
-        { mpz_fdiv_qr(sr,rem_sr,sr,GB); mpz_fdiv_q(rem_sr,rem_sr,MB); strcpy(srj,"GB"); bigsr=1;}
-      else if (mpz_cmp(sr,MBborder)>0)
-        { mpz_fdiv_qr(sr,rem_sr,sr,MB); mpz_fdiv_q(rem_sr,rem_sr,kB); strcpy(srj,"MB"); bigsr=1;}
-      else if (mpz_cmp(sr,kBborder)>0)
-        { mpz_fdiv_qr(sr,rem_sr,sr,kB); strcpy(srj,"kB"); bigsr=1;}
-      else
-        strcpy(srj,"B");
-      if (mpz_cmp(st,PBborder)>0)
-        { mpz_fdiv_qr(st,rem_st,st,PB); mpz_fdiv_q(rem_st,rem_st,TB); strcpy(stj,"PB"); bigst=1;}
-      else if (mpz_cmp(st,TBborder)>0)
-        { mpz_fdiv_qr(st,rem_st,st,TB); mpz_fdiv_q(rem_st,rem_st,GB); strcpy(stj,"TB"); bigst=1;}
-      else if (mpz_cmp(st,GBborder)>0)
-        { mpz_fdiv_qr(st,rem_st,st,GB); mpz_fdiv_q(rem_st,rem_st,MB); strcpy(stj,"GB"); bigst=1;}
-      else if (mpz_cmp(st,MBborder)>0)
-        { mpz_fdiv_qr(st,rem_st,st,MB); mpz_fdiv_q(rem_st,rem_st,kB); strcpy(stj,"MB"); bigst=1;}
-      else if (mpz_cmp(st,kBborder)>0)
-        { mpz_fdiv_qr(st,rem_st,st,kB); strcpy(stj,"kB");bigst=1;}
-      else
-        strcpy(stj,"B");
-      if (mpz_cmp(r,PBborder)>0)
-        { mpz_fdiv_qr(r,rem_r,r,PB); mpz_fdiv_q(rem_r,rem_r,TB); strcpy(rj,"PB/s"); bigr=1;}
-      else if (mpz_cmp(r,TBborder)>0)
-        { mpz_fdiv_qr(r,rem_r,r,TB); mpz_fdiv_q(rem_r,rem_r,GB); strcpy(rj,"TB/s"); bigr=1;}
-      else if (mpz_cmp(r,GBborder)>0)
-        { mpz_fdiv_qr(r,rem_r,r,GB); mpz_fdiv_q(rem_r,rem_r,MB); strcpy(rj,"GB/s"); bigr=1;}
-      else if (mpz_cmp(r,MBborder)>0)
-        { mpz_fdiv_qr(r,rem_r,r,MB); mpz_fdiv_q(rem_r,rem_r,kB); strcpy(rj,"MB/s"); bigr=1;}
-      else if (mpz_cmp(r,kBborder)>0)
-        { mpz_fdiv_qr(r,rem_r,r,kB); strcpy(rj,"kB/s"); bigr=1;}
-      else
-        strcpy(rj,"B/s");
-      if (mpz_cmp(t,PBborder)>0)
-        { mpz_fdiv_qr(t,rem_t,t,PB); mpz_fdiv_q(rem_t,rem_t,TB); strcpy(tj,"PB/s"); bigt=1;}
-      else if (mpz_cmp(t,TBborder)>0)
-        { mpz_fdiv_qr(t,rem_t,t,TB); mpz_fdiv_q(rem_t,rem_t,GB); strcpy(tj,"TB/s"); bigt=1;}
-      else if (mpz_cmp(t,GBborder)>0)
-        { mpz_fdiv_qr(t,rem_t,t,GB); mpz_fdiv_q(rem_t,rem_t,MB); strcpy(tj,"GB/s"); bigt=1;}
-      else if (mpz_cmp(t,MBborder)>0)
-        { mpz_fdiv_qr(t,rem_t,t,MB); mpz_fdiv_q(rem_t,rem_t,kB); strcpy(tj,"MB/s"); bigt=1;}
-      else if (mpz_cmp(t,kBborder)>0)
-        { mpz_fdiv_qr(t,rem_t,t,kB); strcpy(tj,"kB/s"); bigt=1;}
-      else
-        strcpy(tj,"B/s");
+      if (printrt){
+        if (mpz_cmp(sr,PBborder)>0)
+          { mpz_fdiv_qr(sr,rem_sr,sr,PB); mpz_fdiv_q(rem_sr,rem_sr,TB); strcpy(srj,"PB"); bigsr=1;}
+        else if (mpz_cmp(sr,TBborder)>0)
+          { mpz_fdiv_qr(sr,rem_sr,sr,TB); mpz_fdiv_q(rem_sr,rem_sr,GB); strcpy(srj,"TB"); bigsr=1;}
+        else if (mpz_cmp(sr,GBborder)>0)
+          { mpz_fdiv_qr(sr,rem_sr,sr,GB); mpz_fdiv_q(rem_sr,rem_sr,MB); strcpy(srj,"GB"); bigsr=1;}
+        else if (mpz_cmp(sr,MBborder)>0)
+          { mpz_fdiv_qr(sr,rem_sr,sr,MB); mpz_fdiv_q(rem_sr,rem_sr,kB); strcpy(srj,"MB"); bigsr=1;}
+        else if (mpz_cmp(sr,kBborder)>0)
+          { mpz_fdiv_qr(sr,rem_sr,sr,kB); strcpy(srj,"kB"); bigsr=1;}
+        else
+          strcpy(srj,"B");
+        if (mpz_cmp(st,PBborder)>0)
+          { mpz_fdiv_qr(st,rem_st,st,PB); mpz_fdiv_q(rem_st,rem_st,TB); strcpy(stj,"PB"); bigst=1;}
+        else if (mpz_cmp(st,TBborder)>0)
+          { mpz_fdiv_qr(st,rem_st,st,TB); mpz_fdiv_q(rem_st,rem_st,GB); strcpy(stj,"TB"); bigst=1;}
+        else if (mpz_cmp(st,GBborder)>0)
+          { mpz_fdiv_qr(st,rem_st,st,GB); mpz_fdiv_q(rem_st,rem_st,MB); strcpy(stj,"GB"); bigst=1;}
+        else if (mpz_cmp(st,MBborder)>0)
+          { mpz_fdiv_qr(st,rem_st,st,MB); mpz_fdiv_q(rem_st,rem_st,kB); strcpy(stj,"MB"); bigst=1;}
+        else if (mpz_cmp(st,kBborder)>0)
+          { mpz_fdiv_qr(st,rem_st,st,kB); strcpy(stj,"kB");bigst=1;}
+        else
+          strcpy(stj,"B");
+        if (mpz_cmp(r,PBborder)>0)
+          { mpz_fdiv_qr(r,rem_r,r,PB); mpz_fdiv_q(rem_r,rem_r,TB); strcpy(rj,"PB/s"); bigr=1;}
+        else if (mpz_cmp(r,TBborder)>0)
+          { mpz_fdiv_qr(r,rem_r,r,TB); mpz_fdiv_q(rem_r,rem_r,GB); strcpy(rj,"TB/s"); bigr=1;}
+        else if (mpz_cmp(r,GBborder)>0)
+            { mpz_fdiv_qr(r,rem_r,r,GB); mpz_fdiv_q(rem_r,rem_r,MB); strcpy(rj,"GB/s"); bigr=1;}
+        else if (mpz_cmp(r,MBborder)>0)
+          { mpz_fdiv_qr(r,rem_r,r,MB); mpz_fdiv_q(rem_r,rem_r,kB); strcpy(rj,"MB/s"); bigr=1;}
+        else if (mpz_cmp(r,kBborder)>0)
+          { mpz_fdiv_qr(r,rem_r,r,kB); strcpy(rj,"kB/s"); bigr=1;}
+        else
+          strcpy(rj,"B/s");
+        if (mpz_cmp(t,PBborder)>0)
+          { mpz_fdiv_qr(t,rem_t,t,PB); mpz_fdiv_q(rem_t,rem_t,TB); strcpy(tj,"PB/s"); bigt=1;}
+          else if (mpz_cmp(t,TBborder)>0)
+          { mpz_fdiv_qr(t,rem_t,t,TB); mpz_fdiv_q(rem_t,rem_t,GB); strcpy(tj,"TB/s"); bigt=1;}
+        else if (mpz_cmp(t,GBborder)>0)
+          { mpz_fdiv_qr(t,rem_t,t,GB); mpz_fdiv_q(rem_t,rem_t,MB); strcpy(tj,"GB/s"); bigt=1;}
+        else if (mpz_cmp(t,MBborder)>0)
+          { mpz_fdiv_qr(t,rem_t,t,MB); mpz_fdiv_q(rem_t,rem_t,kB); strcpy(tj,"MB/s"); bigt=1;}
+        else if (mpz_cmp(t,kBborder)>0)
+          { mpz_fdiv_qr(t,rem_t,t,kB); strcpy(tj,"kB/s"); bigt=1;}
+        else
+          strcpy(tj,"B/s");
+      }
       // print
       if (mpz_cmp_ui(timer,0)>0 && printrt){
         // date
@@ -360,7 +317,7 @@ int main(int argc, char* argv[]) {
         }
         // interface
         printf("%s",interface);
-        // counter/timers
+        // counter to timers
         if (f_timer){
           ui_sec = mpz_fdiv_q_ui(days,timer,DAY_IN_SEC);
           ui_hours = ui_sec/(DAY_IN_SEC/24);
@@ -369,17 +326,13 @@ int main(int argc, char* argv[]) {
           ui_sec = ui_sec-(ui_minutes*60);
           printf("[");
           if (mpz_cmp_ui(days,0)>0)
-            //gmp_printf("%Zd%s",days,"d");
             gmp_printf("%Zd%s%02u%s%02u%s%02u]",days,"d",ui_hours,"h",ui_minutes,"m",ui_sec);
           else if (ui_hours)
-            //printf("%u%s",ui_hours,"h");
             printf("%u%s%02u%s%02u]",ui_hours,"h",ui_minutes,"m",ui_sec);
           else if (ui_minutes)
-            //printf("%u%s",ui_minutes,"m");
             printf("%u%s%02u]",ui_minutes,"m",ui_sec);
           else
             printf("%02u]",ui_sec);
-          //gmp_printf("[%Zd%s%u%s%u%s%02u]",days,"d",ui_hours,"h",ui_minutes,"m",ui_sec);
         }
         else
           gmp_printf("[%Zd]",timer);
@@ -419,8 +372,6 @@ int main(int argc, char* argv[]) {
         // end
         printf("\n");
       }
-      //lseek(filedr,(off_t)0,SEEK_SET);
-      //lseek(filedt,(off_t)0,SEEK_SET);
       // cur to prev
       mpz_set(aa,a);
       mpz_set(bb,b);
@@ -429,8 +380,6 @@ int main(int argc, char* argv[]) {
     sleep(1);
     mpz_add_ui(timer,timer,1);
   }
-  //close(filedr);
-  //close(filedt);
   nl_socket_free(nlsocket);
   free_num();
   printf("exit\n");
@@ -439,8 +388,6 @@ int main(int argc, char* argv[]) {
 
 void sig_stop(int sig)
 {
-  //close(filedr);
-  //close(filedt);
   nl_socket_free(nlsocket);
   free_num();
   printf("Exit\n");
@@ -538,4 +485,5 @@ void print_usage(){
 \n", stdout);
   printf("%s is a Linux network interface rx/tx status.\n",PROG_NAME);
 }
+
 
